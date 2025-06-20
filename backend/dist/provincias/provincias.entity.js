c410230c4c5ab45df89b5e65782f1d80aee0d34f
Mateo Pretelli <86113904+mateopretelli@users.noreply.github.com>
2025-06-20 20:40:22 -0300

    actualizacion del /dist

--- backend/dist/provincias/provincias.entity.js
+++
@@ -1,7 +0,0 @@
-export class Provincia {
-    constructor(codProvincia, name) {
-        this.codProvincia = codProvincia;
-        this.name = name;
-    }
-}
-//# sourceMappingURL=provincias.entity.js.map
