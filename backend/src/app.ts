cebf98a9e5639429936b51aaa279caf3bb422321
Genaro05 <genaro20038@gmail.com>
2025-06-24 15:09:41 -0300

    personRouter added to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { provinceRouter } from './provinces/provinces.routes.js'
+import { personRouter } from './people/people.routes.js'
+
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/provinces', provinceRouter)
+app.use('/api/people', personRouter)
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' })
