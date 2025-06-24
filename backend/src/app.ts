89dac345f6edfd2431d668e2d510eefc6707b023
LucaOlivieri03 <lucaolivieri03@gmail.com>
2025-06-24 15:40:54 -0300

    Se importó personRouter para evitar conflictos en el merge

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { provinceRouter } from './provinces/provinces.routes.js'
 import { cityRouter} from './cities/cities.routes.js'
+import { personRouter } from './people/people.routes.js'
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/provinces', provinceRouter)
 app.use('/api/cities', cityRouter)
+app.use('/api/people', personRouter)
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' })
