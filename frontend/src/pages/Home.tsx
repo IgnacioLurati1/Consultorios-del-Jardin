d0261971514dec6ddc787cf416a337f72a7c8824
Rod-Alan <alanrodriguez1893@gmail.com>
2025-07-12 16:09:59 -0300

    creation of the components folder

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { LeftArrow, RightArrow, PresentationCard } from "../components/homecomponents";
+
 
 import carousel1 from "../assets/carouselPhotos/carousel1.jpg";
 import carousel2 from "../assets/carouselPhotos/carousel2.jpg";
@@ -12,61 +13,6 @@ import presentationPhoto2 from "../assets/presentationPhotos/presentationPhoto2.
 import presentationPhoto3 from "../assets/presentationPhotos/presentationPhoto3.jpg";
 
 import "../styles/Home.css";
-
-// ======= Carousel arrows =======
-
-const LeftArrow = () => (
-  <svg
-    width="36"
-    height="36"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="black"
-    strokeWidth="4"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <polyline points="16 20 8 12 16 4" />
-  </svg>
-);
-
-const RightArrow = () => (
-  <svg
-    width="36"
-    height="36"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="black"
-    strokeWidth="4"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <polyline points="8 20 16 12 8 4" />
-  </svg>
-);
-
-// ======= Components Cards =======
-
-function PresentationCard({
-  to,
-  image,
-  title,
-}: {
-  to: string;
-  image: string;
-  title: string;
-}) {
-  return (
-    <Link to={to} className="card-link">
-      <div className="card">
-        <img src={image} alt={title} />
-        <h3>{title}</h3>
-      </div>
-    </Link>
-  );
-}
-
-// ======= Home =======
 
 export function Home() {
   const images: string[] = [carousel1, carousel2, carousel3, carousel4, carousel5];
@@ -127,7 +73,6 @@ export function Home() {
         <p>Texto de presentación</p>
         <p>Texto de presentación</p>
         <p>Texto de presentación</p>
-
 
         <div className="cards-container">
           <PresentationCard to="/turnos" image={presentationPhoto1} title="Quiero atenderme" />
