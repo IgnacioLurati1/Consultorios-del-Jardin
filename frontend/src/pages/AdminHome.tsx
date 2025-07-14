95c96f060e0c1ce653b3c71deed6049ad36e1af1
IgnacioLurati1 <ilurati@frro.utn.edu.ar>
2025-07-14 16:54:25 -0300

    Added new NavZone component and fixed responsive issues

--- frontend/src/pages/AdminHome.tsx
+++ frontend/src/pages/AdminHome.tsx
@@ -1,33 +1,37 @@
 import "../styles/AdminHome.css";
 import {AdminCard} from "../components/AdminCard";
 import Logo from '../assets/LogoRecortado.png';
+import {NavZone} from "../components/NavZone";
 
-
+// FALTA VALIDACIONES DE SEGURIDAD EN EL ENRUTAMIENTO!!!!
 export function AdminHome() {
   return (
-    <div className ="admin-home">
-        <h1>Admin Home</h1>
+     
+    <div className ="admin-home"> 
+        <NavZone title="Inicio de sesión" />
+
         <div className="options-container">
           <AdminCard
             title="Administrar provincias"
             description="Añadir, eliminar o modificar provincias."
             imageUrl={Logo}
-            link="/admin/provinces"
+            link="/provincesAdmin"
           />
           <AdminCard
             title="Administrar localidades"
             description="Añadir, eliminar o modificar localidades."
             imageUrl={Logo}
-            link="/admin/cities"
+            link="/citiesAdmin"
           />
           <AdminCard
             title="Administrar usuarios"
             description="Añadir, eliminar o modificar usuarios."
             imageUrl={Logo}
-            link="/admin/people"
+            link="/peopleAdmin"
           />
     
         </div>
     </div>
+
   );
 }
