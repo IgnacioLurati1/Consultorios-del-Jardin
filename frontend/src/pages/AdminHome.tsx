bcb1be2d0283f2ac89e2c873efe4ca097f7f3235
IgnacioLurati1 <ilurati@frro.utn.edu.ar>
2025-07-14 15:57:56 -0300

    AdminCards and link references

--- frontend/src/pages/AdminHome.tsx
+++ frontend/src/pages/AdminHome.tsx
@@ -1,11 +1,33 @@
 import "../styles/AdminHome.css";
+import {AdminCard} from "../components/AdminCard";
+import Logo from '../assets/LogoRecortado.png';
 
 
 export function AdminHome() {
   return (
-    <div className = "admin-home">
-      <h1>Admin Home</h1>
-      <p>Welcome to the admin dashboard!</p>
+    <div className ="admin-home">
+        <h1>Admin Home</h1>
+        <div className="options-container">
+          <AdminCard
+            title="Administrar provincias"
+            description="Añadir, eliminar o modificar provincias."
+            imageUrl={Logo}
+            link="/admin/provinces"
+          />
+          <AdminCard
+            title="Administrar localidades"
+            description="Añadir, eliminar o modificar localidades."
+            imageUrl={Logo}
+            link="/admin/cities"
+          />
+          <AdminCard
+            title="Administrar usuarios"
+            description="Añadir, eliminar o modificar usuarios."
+            imageUrl={Logo}
+            link="/admin/people"
+          />
+    
+        </div>
     </div>
   );
 }
