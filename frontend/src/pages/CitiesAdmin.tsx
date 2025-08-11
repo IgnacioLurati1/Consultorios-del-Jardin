24727be17c6f842e360cec69523ad7d41195ac79
LucaOlivieri03 <lucaolivieri03@gmail.com>
2025-08-11 10:02:50 -0300

    loading and error added

--- frontend/src/pages/CitiesAdmin.tsx
+++ frontend/src/pages/CitiesAdmin.tsx
@@ -15,25 +15,46 @@ export function CitiesAdmin() {
         province: Province; 
     }
 
-    const [provinces, setProvinces] = useState([]);
+    const [provinces, setProvinces] = useState<Province[]>([]);
     const [cities, setCities] = useState<City[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     const [nameCity, setNameCity] = useState("");
     const [selectedProvince, setSelectedProvince] = useState("");
+
 
     useEffect(() => {
     fetch("/api/provinces")
       .then(res => res.json())
-      .then(data => setProvinces(data.data)) 
-      .catch(err => console.error("Error cargando provincias:", err));
+      .then(data => {
+        setProvinces(data.data)
+        setLoading(false); })
+      .catch(err => {
+        setLoading(false);
+        setError(err.message);console.error("Error cargando provincias:", err)});
     }, []);
 
     useEffect(() => {
     fetch("/api/cities")
       .then(res => res.json())
-      .then(data => setCities(data.data))
-      .catch(err => console.error("Error cargando ciudades:", err));
+      .then(data => {
+        setCities(data.data)
+        setLoading(false);
+      })
+      .catch(err => {
+        setLoading(false);
+        setError(err.message);
+        console.error("Error cargando ciudades:", err)});
     }, []);
   
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
     function DeleteCity(id: string) {
         fetch(`/api/cities/${id}`, { method: 'DELETE' })
         .then(res => {
