4362aa97f6d9006d176462fa91f7b9c5de285427
IgnacioLurati1 <ilurati@frro.utn.edu.ar>
2025-08-15 01:45:16 -0300

    Tested firebase for the first time (ALLELUYA)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+
 import 'reflect-metadata'
 import express from 'express'
 import { provinceRouter } from './provinces/provinces.routes.js'
@@ -7,6 +8,25 @@ import { officeRouter } from './offices/offices.routes.js'
 import { roomRouter } from './rooms/rooms.routes.js'
 import { orm, syncSchema } from './shared/db/orm.js'
 import { RequestContext } from '@mikro-orm/core'
+import admin from "./config/firebase.js";
+
+import { Request, Response, NextFunction } from "express";
+
+export async function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token requerido" });
+  }
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    (req as any).user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Token inválido" });
+  }
+}
+
 
 const app = express()
 app.use(express.json())
@@ -15,7 +35,7 @@ app.use((req, res, next) => {
   RequestContext.create(orm.em, next)
 })
 
-app.use('/api/provinces', provinceRouter)
+app.use('/api/provinces', verifyToken, provinceRouter)
 app.use('/api/cities', cityRouter)
 app.use('/api/people', personRouter)
 app.use('/api/offices', officeRouter)
