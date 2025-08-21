fcbac2573590304ad848e8a60b5bfc6a833f627e
LucaOlivieri03 <lucaolivieri03@gmail.com>
2025-08-21 16:50:42 -0300

    Changed cities controller used in frontend

--- backend/src/cities/cities.controller.ts
+++ backend/src/cities/cities.controller.ts
@@ -1,12 +1,9 @@
 import { Request, Response, NextFunction} from 'express'
-import { orm } from '../shared/db/orm.js'
-import { City } from './cities.entity.js'
-import { error } from 'console'
-import { create } from 'domain'
+import { CityService } from './cities.service.js'
 
-const em = orm.em
+const cityService = new CityService();
 
-function sanitizeCityInput(req: Request, res: Response, next: NextFunction) {
+export function sanitizeCityInput(req: Request, res: Response, next: NextFunction) {
   req.body.sanitizedInput = {
     nameCity: req.body.nameCity?.toString().trim(),
     idCity: req.body.idCity,
@@ -25,7 +22,7 @@ function sanitizeCityInput(req: Request, res: Response, next: NextFunction) {
   next()
 }
 
-function validateCityData(req: Request, res:Response, next: NextFunction) {
+export function validateCityData(req: Request, res:Response, next: NextFunction) {
   const { sanitizedInput } = req.body
   const errors: string[] = []
 
@@ -50,7 +47,7 @@ function validateCityData(req: Request, res:Response, next: NextFunction) {
   next()
 }
 
-async function validateCreateAndUpdateCityInput(req: Request, res: Response, next: NextFunction) {
+export async function validateCreateAndUpdateCityInput(req: Request, res: Response, next: NextFunction) {
   const {sanitizedInput} = req.body
   const errors: string[] = []
 
@@ -63,29 +60,17 @@ async function validateCreateAndUpdateCityInput(req: Request, res: Response, nex
   }
 
   if(sanitizedInput.nameCity && sanitizedInput.province){
-    try {
-      const whereClause: any = { 
-        nameCity: { $like: sanitizedInput.nameCity.trim() },  //Creo una ciudad con el mismo nombre, pero insensible a mayusculas y minusculas y misma prov
-        province: sanitizedInput.province 
-      }
-
-      const cityId = req.params.idCity ? Number.parseInt(req.params.idCity) : null
-
-      if (cityId) {
-        whereClause.idCity = { $ne: cityId } //le agrego el id con $ne que es un operador de MikroORM que significa "not equal", busca pero ignora la que tiene ese id
-      }
-
-
-      const existingCity = await em.findOne(City, whereClause) 
+      try {
+      const cityId = req.params.idCity ? Number(req.params.idCity) : undefined;
+      const existingCity = await cityService.cityExistsWithNameAndProvince(sanitizedInput.nameCity, sanitizedInput.province, cityId);
 
       if (existingCity) {
         errors.push('Ya existe una ciudad con el mismo nombre en la misma provincia')
       }
-  } catch(error: any) {
+    }catch(error: any) {
       errors.push('Error al validar ciudades con mismo nombre')
     }
   }
-
 
   if (errors.length > 0) {
     const errorMessage = errors.join(', ')
@@ -94,9 +79,10 @@ async function validateCreateAndUpdateCityInput(req: Request, res: Response, nex
     });
   }
   next();
+
 }
 
-function validateUpdateCityInput(req: Request, res: Response, next: NextFunction) {
+export function validateUpdateCityInput(req: Request, res: Response, next: NextFunction) {
   const { sanitizedInput } = req.body;
   
   if (!sanitizedInput.nameCity && !sanitizedInput.province && sanitizedInput.active === undefined) {
@@ -108,19 +94,19 @@ function validateUpdateCityInput(req: Request, res: Response, next: NextFunction
   next();
 }
 
-async function findAll(req: Request, res: Response) {
+export async function findAll(req: Request, res: Response) {
   try {
-    const cities = await em.find(City, {}, {populate: ['province']})
+    const cities = await cityService.findAllCities()
     res.status(200).json({ message: 'find all cities', data: cities })
   }catch (error : any) {
     res.status(500).json({ message: error.message })
   }
 }
 
-async function findOne(req: Request, res: Response) {
+export async function findOne(req: Request, res: Response) {
   try{
     const id = Number.parseInt(req.params.idCity)
-    const city = await em.findOneOrFail(City, { idCity : id })
+    const city = await cityService.findCityById(id)
     res
       .status(200)
       .json({ message: 'found one city', data: city })
@@ -129,28 +115,30 @@ async function findOne(req: Request, res: Response) {
   }
 }
 
-async function add(req: Request, res: Response) {
+export async function add(req: Request, res: Response) {
   try{
-  const city = em.create(City, req.body.sanitizedInput)
-  await em.flush()
-  const createdCity = await em.findOne(City, { idCity: city.idCity }, {populate: ['province']})
-  res.status(201).json({ message: 'City created successfully', data: createdCity })
+  const city = cityService.createCity(req.body.sanitizedInput)
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
 }
 
-async function update(req: Request, res: Response) {
+export async function update(req: Request, res: Response) {
   try{
     const id = Number.parseInt(req.params.idCity)
-    const city = await em.findOneOrFail(City,  { idCity : id })
-    em.assign(city, req.body.sanitizedInput)
-    await em.flush() 
-    const updatedCity = await em.findOneOrFail(City, { idCity: id }, {populate: ['province']})
+    const updatedCity = cityService.updateCity(id, req.body.sanitizedInput)
     res.status(200).json({ message: 'City updated successfully', data: updatedCity })
   }catch (error: any) {
     res.status(500).json({ message: error.message })
   }
 }
 
-export {sanitizeCityInput, validateCityData, validateCreateAndUpdateCityInput, validateUpdateCityInput, findAll, findOne, add, update}
+export async function toggleCityState(req: Request, res: Response) {
+  try {
+    const idCity = Number(req.params.idCity);
+    const city = await cityService.toggleCityState(idCity);
+    res.status(200).json({ message: 'Estado de la ciudad y offices actualizado', data: city });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+}
