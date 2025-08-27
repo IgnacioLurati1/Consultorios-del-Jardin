42a53a655c9c6004864ae30a2e518ff1cc47052f
Rod-Alan <alanrodriguez1893@gmail.com>
2025-08-27 15:02:04 -0300

    add OfficeLabelStyle.css

--- frontend/src/pages/adminCRUDS/adminOffices/OfficeLabel.tsx
+++ frontend/src/pages/adminCRUDS/adminOffices/OfficeLabel.tsx
@@ -3,13 +3,14 @@ import "../CRUDSLabel.css";
 
 export function OfficeLabel(office: {description: string, id: string, openingTime: string, closingTime: string, city: string, provinces: string , onDelete: () => void, onEdit: () => void, active: boolean}) {
   return (
-    <div className={`${office.active ? 'crud-label-green' : 'crud-label-red'} crud-label city-label`}>
-      <p className="crud-name"><strong>ID: {office.id}</strong></p>
-      <p className= "crud-name">{office.description}
-      <br></br>
-      de {office.openingTime}hs a {office.closingTime}hs
-      </p>
-      <p className="crud-id">{office.city}, {office.provinces}.</p>  
+    <div className={`${office.active ? 'crud-label-green' : 'crud-label-red'} crud-label office-label`}>
+      <div>
+      <p className="crud-name"><strong>ID: {office.id}</strong> {office.description}</p>
+      <p className= "crud-name"> De {office.openingTime}hs a {office.closingTime}hs </p>
+      </div>
+      <div>
+        <p className="crud-id">{office.city}, {office.provinces}</p>
+      </div>
     </div>
   );
 }
