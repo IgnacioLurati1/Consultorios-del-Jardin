4975003bfdfc69f7152427840363ccd7d5266f39
Rod-Alan <alanrodriguez1893@gmail.com>
2025-08-27 13:12:27 -0300

    visual enhancement of lbael

--- frontend/src/pages/adminCRUDS/adminOffices/OfficeLabel.tsx
+++ frontend/src/pages/adminCRUDS/adminOffices/OfficeLabel.tsx
@@ -3,12 +3,13 @@ import "../CRUDSLabel.css";
 
 export function OfficeLabel(office: {description: string, id: string, openingTime: string, closingTime: string, city: string, provinces: string , onDelete: () => void, onEdit: () => void, active: boolean}) {
   return (
-    <div className={`${office.active ? 'crud-label-green' : 'crud-label-red'} .city-label .crud-name`}>
-      <li className="crud-create-entity-input">
-      <p className="crud-name">ID {office.id}. {office.description}</p> 
-      <p className="crud-name">Horario: de {office.openingTime}hs a {office.closingTime}hs</p>     
-      <p className="crud-name">{office.city}, {office.provinces}.</p>
-      </li>
+    <div className={`${office.active ? 'crud-label-green' : 'crud-label-red'} crud-label city-label`}>
+      <p className="crud-name"><strong>ID: {office.id}</strong></p>
+      <p className= "crud-name">{office.description}
+      <br></br>
+      de {office.openingTime}hs a {office.closingTime}hs
+      </p>
+      <p className="crud-id">{office.city}, {office.provinces}.</p>  
     </div>
   );
 }
