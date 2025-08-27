8796c6c35b7f287771e0d455597fa495b748f9a7
Rod-Alan <alanrodriguez1893@gmail.com>
2025-08-27 19:13:51 -0300

    new view

--- frontend/src/pages/adminCRUDS/adminOffices/OfficeLabel.tsx
+++ frontend/src/pages/adminCRUDS/adminOffices/OfficeLabel.tsx
@@ -1,23 +1,10 @@
 import "../CRUDSLabel.css";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faLocationDot} from '@fortawesome/free-solid-svg-icons';
-
-
 
 export function OfficeLabel(office: {description: string, id: string, openingTime: string, closingTime: string, city: string, onDelete: () => void, onEdit: () => void, active: boolean}) {
   return (
-    <div className={`${office.active ? 'crud-label-green' : 'crud-label-red'} crud-label room-label`}>
-      <div className="room-label-container">
-        <div className="room-label-upper">
-          <p className="crud-id"> ID: {office.id}</p>
-          <p className="crud-name">{office.description}</p>
-        </div>
-        <hr />
-        <div className="room-label-lower">
-          <p className="crud-name">De {office.openingTime}hs a {office.closingTime}hs</p>
-          <p className="crud-name">{office.city}<FontAwesomeIcon className="icon-location"icon={faLocationDot}/></p>
-        </div>
-      </div>
+    <div className={`${office.active ? 'crud-label-green' : 'crud-label-red'} crud-label office-label`}>
+        <p className="crudName">{office.description}</p>
+        <p className="crud-id">ID: {office.id}</p>
     </div>
   );
 }
