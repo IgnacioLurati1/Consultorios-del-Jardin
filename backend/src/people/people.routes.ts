e37b8b9351e61257c92554d3e386e362ceb0abd7
Mateo Pretelli <86113904+mateopretelli@users.noreply.github.com>
2025-08-30 20:51:12 -0300

    Login routes implemented

--- backend/src/people/people.routes.ts
+++ backend/src/people/people.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { sanitizePersonInput, findAll, findOne, add, update, remove } from './people.controller.js'
+import { sanitizePersonInput, findAll, findOne, add, update, loginWithEmailAndPassword } from './people.controller.js'
 
 export const personRouter = Router()
 
 personRouter.get('/', findAll)
 personRouter.get('/:email', findOne)
 personRouter.post('/', sanitizePersonInput, add)
+personRouter.post('/login', loginWithEmailAndPassword)
 personRouter.put('/:email', sanitizePersonInput, update)
 personRouter.patch('/:email', sanitizePersonInput, update)
