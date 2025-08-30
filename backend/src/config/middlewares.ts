cce8223bb4cdc26db5e04c9cda30083f83b66adf
Mateo Pretelli <86113904+mateopretelli@users.noreply.github.com>
2025-08-30 16:55:50 -0300

    Middleware JWT actualization

--- backend/src/config/middlewares.ts
+++ backend/src/config/middlewares.ts
@@ -10,14 +10,19 @@ interface RequestWithUser extends Request {
 
 export async function verifyToken(req: RequestWithUser, res: Response, next: NextFunction) {
     const token = req.headers.authorization?.split(' ')[1]; //Del header, nos quedamos con el authorization: Bearer, el ? pregunta si existe, de lo contrario undefined y el split separa el bearer del token y nos quedamos con este ultimo
-    if (!token) return res.status(401).send('Unauthorized');
+    if (!token) return res.status(401).json({ message: 'No autorizado' });
 
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET as jwt.Secret);
         req.user = decodedToken;
         next();
     } catch (error) {
-        console.error('Error verifying token:', error);
-        res.status(401).send('Token inválido');
+        if (error instanceof jwt.TokenExpiredError) {
+            console.error('Error verifying token:', error);
+            res.status(401).json({ message: 'Token expirado' }); // si el token expira, devuelve error
+        } else {
+            console.error('Error verifying token:', error);
+            res.status(401).json({ message: 'Token inválido' });
+        }
     }
 }
