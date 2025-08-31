924379dd84e0a69e64ec28215dce45dbf1cd1532
IgnacioLurati1 <ilurati@frro.utn.edu.ar>
2025-08-31 18:01:25 -0300

    Made crudNav responsive, erased actual page from link options

--- frontend/src/components/crudNav/CrudNav.tsx
+++ frontend/src/components/crudNav/CrudNav.tsx
@@ -1,15 +1,43 @@
 import { Link } from "react-router-dom";
 import "./CrudNav.css";
+import { FaArrowUp } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+function Translate(text: string){
+  const Translation: { [key: string]: string } = {
+    Provinces: "Provincias",
+    Cities: "Localidades",
+    Offices: "Oficinas",
+    Users: "Usuarios",
+    Rooms: "Salas"
+  };
+    return Translation[text];
+}
+
 
 export default function CrudNav() {
+  const initialChilds = ["ProvincesAdmin", "CitiesAdmin", "OfficesAdmin", "RoomsAdmin", "UsersAdmin"];
+  const location = useLocation().pathname.split("/")[2];
+  const [childs, setChilds] = useState<string[]>(initialChilds);
+
+  useEffect(() => {
+    setChilds(initialChilds.filter((child) => child !== location));
+  }, [location]);
+
   return (
-  
   <section className="crud-nav">
+    <div className="crud-nav-up"><FaArrowUp className="crud-nav-arrow" /></div>
     <nav className="crud-nav-items">
-        <div className="crud-nav-item"><Link className="crud-nav-link" to="/AdminHome/ProvincesAdmin">Provinces</Link></div>
-        <div className="crud-nav-item"><Link className="crud-nav-link" to="/AdminHome/CitiesAdmin">Cities</Link></div>
-        <div className="crud-nav-item"><Link className="crud-nav-link" to="/AdminHome/OfficesAdmin">Offices</Link></div>
-        <div className="crud-nav-item"><Link className="crud-nav-link" to="/AdminHome/RoomsAdmin">Rooms</Link></div>
+
+      {childs.map((child) => (
+        <div className="crud-nav-item" key={child}>
+          <Link className="crud-nav-link" to={`/AdminHome/${child}`}>
+            <strong>{Translate(child.replace("Admin", ""))}</strong>
+          </Link>
+        </div>
+
+      ))}
     </nav>
   </section>
   );
