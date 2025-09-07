4b76e133984b9e838ae6ce02ad1a6e880f0ee4fc
Genaro05 <genaro20038@gmail.com>
2025-09-07 10:30:50 -0300

    static working schedules without fectches implemented

--- frontend/src/pages/types.ts
+++ frontend/src/pages/types.ts
@@ -5,24 +5,47 @@ export interface Province {
 }
 
 export interface City {
-        idCity: string;
-        nameCity: string;
-        province: Province;
-        active: boolean;
-    }
+    idCity: string;
+    nameCity: string;
+    province: Province;
+    active: boolean;
+}
 
 export interface Office {
-        idOffice: string;
-        description: string;
-        openingTime: string;
-        closingTime: string;
-        city: City
-        active: boolean;
-    }
+    idOffice: string;
+    description: string;
+    openingTime: string;
+    closingTime: string;
+    city: City
+    active: boolean;
+}
 
 export interface Room {
-        idRoom: string;
-        description: string;
-        office: Office;
-        active: boolean;
-    }
+    idRoom: string;
+    description: string;
+    office: Office;
+    active: boolean;
+}
+
+export interface Person{
+    email: string;
+    docType: string;
+    docNumber: string;
+    name: string;
+    surname: string;
+    phoneNumber: string;
+    password: string;
+    speciality: string;
+    type: string;
+    active: boolean;
+}
+
+export interface Schedule {
+    day: string;
+    initialHour: string;
+    Person: string;
+    Room: string;
+    finalHour: string;
+    state: boolean;
+    allowedType: "simple" | "workshop";
+}
