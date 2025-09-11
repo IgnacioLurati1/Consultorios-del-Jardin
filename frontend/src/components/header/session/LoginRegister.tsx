d6b2a468d9789b8a4e5f97b9800f6e14e366ca5d
Luca Olivieri <lucaolivieri03@gmail.com>
2025-09-11 17:38:22 -0300

    logout button changed to use logout endpoint with axios

--- frontend/src/components/header/session/LoginRegister.tsx
+++ frontend/src/components/header/session/LoginRegister.tsx
@@ -1,21 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../context/AuthContext';
+import api from "../../../axios";
 
 export function LoginRegister() {
   const {token, logout} = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+    api.post('/people/logout', {}, { withCredentials: true })
+  }
 
   return (
     <div className="login-register">
       {token? 
-      <Link to={"/"}>
-      <button className = "login out button-right" onClick={logout}>Cerrar sesión</button>
-      </Link>
+      <button className = "login out button-right" onClick={handleLogout}>Cerrar sesión</button>
       :
       <>
-      <Link to={"/Login"}>
+      <Link to={"/login"}>
         <button className="login button-right">Iniciar Sesión</button>
       </Link>
-      <Link to={"/Register"}>
+      <Link to={"/register"}>
         <button className="register button-right">Registrarse</button>
       </Link>
       </> 
