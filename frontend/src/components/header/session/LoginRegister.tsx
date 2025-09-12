c630716c78fdd2ed616c00d3b37caae8ae1ec07b
IgnacioLurati1 <ilurati@frro.utn.edu.ar>
2025-09-12 18:31:52 -0300

    Fixed register and login using api instead of axios

--- frontend/src/components/header/session/LoginRegister.tsx
+++ frontend/src/components/header/session/LoginRegister.tsx
@@ -1,46 +1,50 @@
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../../context/AuthContext';
-import api from "../../../axios";
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../context/AuthContext";
+import axios from "axios";
+import { useState } from "react";
 
 export function LoginRegister() {
-  const {token, logout} = useAuth();
+  const { token, logout } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
-    navigate('/login');
-    api.post('/people/logout', {}, { withCredentials: true })
-    setSureLogOut(false)
-  }
-  const [sureLogOut, setSureLogOut] =  useState(false);
-  
+    navigate("/login");
+    axios.post("/people/logout", {}, { withCredentials: true });
+    setSureLogOut(false);
+  };
+  const [sureLogOut, setSureLogOut] = useState(false);
+
   return (
     <div className="login-register">
-      {token? 
-      <button className = "login-out button-right" onClick={() => setSureLogOut(true)}>Cerrar sesión</button>
-      :
-      <>
-      <Link to={"/login"}>
-        <button className="login button-right">Iniciar Sesión</button>
-      </Link>
-      <Link to={"/register"}>
-        <button className="register button-right">Registrarse</button>
-      </Link>
-      </> 
-      }
+      {token ? (
+        <button className="login-out button-right" onClick={() => setSureLogOut(true)}>
+          Cerrar sesión
+        </button>
+      ) : (
+        <>
+          <Link to={"/login"}>
+            <button className="login button-right">Iniciar Sesión</button>
+          </Link>
+          <Link to={"/register"}>
+            <button className="register button-right">Registrarse</button>
+          </Link>
+        </>
+      )}
 
-      <div className={`logout-confirmation ${sureLogOut ? 'visible' : ''}`} onClick={() => setSureLogOut(false)}>
+      <div className={`logout-confirmation ${sureLogOut ? "visible" : ""}`} onClick={() => setSureLogOut(false)}>
         <div className="logout-content" onClick={(e) => e.stopPropagation()}>
-           <p>¿Está seguro de que desea cerrar sesión?</p>
+          <p>¿Está seguro de que desea cerrar sesión?</p>
           <div className="logout-buttons">
-            <button className="confirm-logout-button button-right" onClick={handleLogout}>Sí</button>
-            <button className="cancel-logout-button button-right" onClick={() => setSureLogOut(false)}>No</button>
+            <button className="confirm-logout-button button-right" onClick={handleLogout}>
+              Sí
+            </button>
+            <button className="cancel-logout-button button-right" onClick={() => setSureLogOut(false)}>
+              No
+            </button>
           </div>
-          
         </div>
-       
       </div>
     </div>
   );
