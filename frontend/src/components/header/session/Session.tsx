8aa698a56630cb87b3ee7d4b8487504835af85de
Luca Olivieri <lucaolivieri03@gmail.com>
2025-09-12 18:10:55 -0300

    Logout button now open a modal to confirm Account button added with styles

--- frontend/src/components/header/session/Session.tsx
+++ frontend/src/components/header/session/Session.tsx
@@ -5,8 +5,7 @@ import { useAuth } from "../../../context/AuthContext";
 
 export function Session() {
     const {token} = useAuth();
-    //const accion = isLoggedIn ? "Cerrar Sesión" : "Iniciar Sesión";
-    //<button onClick={() => {setIsLoggedIn(!isLoggedIn)}}> {accion} </button>
+
     return (
         <div className="session">
             {
