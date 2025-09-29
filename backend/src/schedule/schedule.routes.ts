b08d6d7d78b50ef47291a6ea44ad77be027a3b76
Luca Olivieri <lucaolivieri03@gmail.com>
2025-09-28 21:54:37 -0300

    deleted verigyAdmin in get people by email to be used in professional schedules

--- backend/src/schedule/schedule.routes.ts
+++ backend/src/schedule/schedule.routes.ts
@@ -16,7 +16,7 @@ export const scheduleRouter = Router();
 //Rutas menos genericas primero
 scheduleRouter.get('/', findAll);
 scheduleRouter.get('/profesional', findByProfesionalLogged); // ruta para horarios del profesional logueado
-scheduleRouter.get('/by-email/:email',verifyAdmin,findByEmail); // ex-ruta para PRUEBAS, ahora usada en horarios ADMIN
+scheduleRouter.get('/by-email/:email',findByEmail); // ex-ruta para PRUEBAS, ahora usada en horarios ADMIN
 scheduleRouter.get('/by-day-hour/:day/:initialHour', findOne);
 scheduleRouter.post('/', sanitizeScheduleInput, add);
 scheduleRouter.put('/', sanitizeScheduleInput, update);
