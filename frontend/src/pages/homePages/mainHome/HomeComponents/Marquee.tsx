6621d1507665c3b382a6dc6d9bd469524dd6e61f
Rod-Alan <alanrodriguez1893@gmail.com>
2025-10-10 18:23:08 -0300

    change of office photos

--- frontend/src/pages/homePages/mainHome/HomeComponents/Marquee.tsx
+++ frontend/src/pages/homePages/mainHome/HomeComponents/Marquee.tsx
@@ -1,25 +1,24 @@
 import "./Marquee.css";
+
 import img1 from "../../../../assets/HomePhotos/HomeOfficesPhotos/office1.jpg";
 import img2 from "../../../../assets/HomePhotos/HomeOfficesPhotos/office2.jpg";
 import img3 from "../../../../assets/HomePhotos/HomeOfficesPhotos/office3.jpg";
 import img4 from "../../../../assets/HomePhotos/HomeOfficesPhotos/office4.jpg";
-import img5 from "../../../../assets/HomePhotos/HomeOfficesPhotos/office5.jpg";
 
 const images = [
-  { src: img1, caption: "Pediatría" },
-  { src: img2, caption: "Cardiología" },
-  { src: img3, caption: "Traumatología" },
-  { src: img4, caption: "Neurología" },
-  { src: img5, caption: "Ginecología" },
+  { src: img1, caption: "Nutrición" },
+  { src: img2, caption: "Psicología" },
+  { src: img3, caption: "Psicopedagogía" },
+  { src: img4, caption: "particulares" },
 ];
 
-export function Marquee() {
-  const tripledImages = [...images, ...images, ...images, ...images, ...images, ...images];
+const repeatedImages = Array(300).fill(images).flat();
 
+export function Marquee() {
   return (
     <div className="marquee-container">
       <div className="marquee-track">
-        {tripledImages.map((img, index) => (
+        {repeatedImages.map((img, index) => (
           <div className="marquee-item" key={index}>
             <img src={img.src} alt={img.caption} />
             <p className="caption">{img.caption}</p>
