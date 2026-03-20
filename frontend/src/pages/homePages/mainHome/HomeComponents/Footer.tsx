379cd7a938d45b9776136ecc4a6d92652b2d2add
Rod-Alan <alanrodriguez1893@gmail.com>
2026-03-20 16:48:02 -0300

    footer year

--- frontend/src/pages/homePages/mainHome/HomeComponents/Footer.tsx
+++ frontend/src/pages/homePages/mainHome/HomeComponents/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export function Footer() {
   return (
     <footer>
-      <p>© 2025 Consultorios del Jardín – Todos los derechos reservados</p>
+      <p>©2026 Consultorios del Jardín – Todos los derechos reservados</p>
     </footer>
   );
 }
