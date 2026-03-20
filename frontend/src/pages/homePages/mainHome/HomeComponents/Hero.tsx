3748573815942e0f65cf827834944e2dc2f81614
Rod-Alan <alanrodriguez1893@gmail.com>
2026-03-20 16:44:37 -0300

    The image and text style of the main page were changed.

--- frontend/src/pages/homePages/mainHome/HomeComponents/Hero.tsx
+++ frontend/src/pages/homePages/mainHome/HomeComponents/Hero.tsx
@@ -8,6 +8,7 @@ export function Hero() {
       style={{ backgroundImage: `url(${homeImg})` }}
     >
       <div className="text-container">
+        <div className="text-blur-bg" />
         <h1 className="titleHome">Consultorios del Jardín</h1>
         <p className="subtitleHome">
           Nuestros mejores lugares para los mejores profesionales
