d7615adc2fbf70ffd721d468f967cb1f7f820f39
Genaro05 <genaro20038@gmail.com>
2026-03-27 13:43:54 -0300

    fixes for cors problem when connecting frontend deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 import { provinceRouter } from "./provinces/provinces.routes.js";
 import { cityRouter } from "./cities/cities.routes.js";
 import { personRouter } from "./people/people.routes.js";
@@ -16,8 +17,17 @@ import { appointmentRouter } from "./appointments/appointments.routes.js";
 import { startReminderJob } from "./jobs/reminder.job.js";
 import { setupSwagger } from './config/swagger.js';
 
+
 const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
+
+app.use(cors({
+  // Reemplaza esto con tu URL real de Vercel
+  origin: ["dsw-autogestora-de-turnos-production.up.railway.app", "http://localhost:5173"], 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"]
+}));
 
 app.use(express.json());
 app.use(cookieParser());
