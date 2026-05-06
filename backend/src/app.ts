519d01d00580fd71b1604e8804a04655ec7d8c1d
IgnacioLurati1 <ilurati@frro.utn.edu.ar>
2026-05-06 17:48:53 -0300

    Added patch to cors policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -39,7 +39,7 @@ app.use(cors({
       callback(new Error("Bloqueado por CORS: Este origen no está permitido"));
     }
   },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"]
 }));
